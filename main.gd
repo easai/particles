b03e3a19fb2af1626dc6f165af5bae8f73978972
easai <aksarih@gmail.com>
2025-07-28 03:39:14 -0400

    add carbon dioxide

--- main.gd
+++ main.gd
@@ -4,9 +4,9 @@ class_name Main
 
 const MOLECULE = preload("res://molecule.tscn")
 const OXYGEN = preload("res://oxygen.tscn")
+const CARBON_DIOXIDE = preload("res://carbon_dioxide.tscn")
 
 static var _vp_r: Rect2
-
 
 static func get_vpr():
 	return _vp_r
@@ -19,12 +19,16 @@ func update_vp() -> void:
 func _ready() -> void:
 	update_vp()
 	get_viewport().size_changed.connect(update_vp)
-	for i in range(10):
-		spawn_molecule()
+	spawn_molecule()
 
 
 func spawn_molecule() -> void:
-	var new_molecule: Molecule = MOLECULE.instantiate()
-	add_child(new_molecule)
-	var new_oxygen: Oxygen = OXYGEN.instantiate()
-	add_child(new_oxygen)	
+	for i in range(7):
+		var new_molecule: Molecule = MOLECULE.instantiate()
+		add_child(new_molecule)
+	for i in range(10):
+		var new_oxygen: Oxygen = OXYGEN.instantiate()
+		add_child(new_oxygen)	
+	for i in range(5):
+		var new_carbon_dioxide: CarbonDioxide = CARBON_DIOXIDE.instantiate()
+		add_child(new_carbon_dioxide)
